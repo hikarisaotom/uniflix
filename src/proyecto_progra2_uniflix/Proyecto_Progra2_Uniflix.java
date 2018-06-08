95b05d291c716f414c08d002105ad681e366b32b
Claudia <clau_cortes10@hotmail.com>
2018-06-08 09:41:32 -0600

    Primer avance

---
+++ src/proyecto_progra2_uniflix/Proyecto_Progra2_Uniflix.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_progra2_uniflix;
+
+/**
+ *
+ * @author Claudia Cortes
+ */
+public class Proyecto_Progra2_Uniflix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
